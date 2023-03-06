27af24ffd0393580c273962e61a42d48c13dc4ce
dil98 <dilip.dhital20@gmail.com>
2023-03-06 13:51:01 -0500

    new changes

---
+++ StatusSolution/Status/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Status;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/status", () =>
+{
+    var statusMessage = new StatusMessage(Guid.NewGuid(), "Looks Good", DateTimeOffset.Now);
+    return Results.Ok(statusMessage);
+});
+
+
+
+app.Run();
