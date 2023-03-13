48c1ec60050d7cd2e68dd3a60f0364af0d6021a7
Saleh <saleh6877@gmail.com>
2023-03-13 13:52:58 -0400

    Mine

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+// this is a decorator, just like attributes [HttpGet("/tacos")], [Fact]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
 }
