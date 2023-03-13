ce0966f02c90f25648e05faea5fc78164306e3f9
dil98 <dilip.dhital20@gmail.com>
2023-03-13 15:22:31 -0400

    update route

--- stringcalculator-master/StringCalculatorActionTest.cs
+++ stringcalculator-master/StringCalculatorActionTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Castle.Core.Logging;
 
 namespace StringCalculator
 {
     internal class StringCalculatorActionTest
     {
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("42", 42)]
+        [InlineData("", "0")]
+
+        public void ResultsAreLogged(string numbers, string expected)
+        {
+            var mockedLogger = new Mock<ILogger>();
+            var calculator = new StringCalculator(mockedLogger.Object);
+
+
+            calculator.Add(numbers);
+
+            mockedLogger.Verify(m => m.Write(expected), Times.Once);
+
+        }
     }
 }
--- stringcalculator-master/StringCalculatorTests.cs
+++ stringcalculator-master/StringCalculatorTests.cs
@@ -1,15 +1,32 @@
-﻿
-namespace StringCalculator;
+﻿using Castle.Core.Logging;
 
-public class StringCalculatorTests
+namespace StringCalculator; public class StringCalculatorTests
 {
+    private readonly StringCalculator _calculator;
+    public StringCalculatorTests()
+    {
+        _calculator = new StringCalculator(new Mock<ILogger>(),object);
+    }
     [Fact]
     public void EmptyStringReturnsZero()
     {
-        var calculator = new StringCalculator();
-
-        var result = calculator.Add("");
-
-        Assert.Equal(0, result);
+        var result = _calculator.Add(""); Assert.Equal(0, result);
+    }
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("42", 42)]
+    [InlineData("108", 108)]
+    public void SingleDigit(string numbers, int expected)
+    {
+        var result = _calculator.Add(numbers); Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+    [InlineData("108\n1", 109)]
+    [InlineData("1\n2,3", 6)]
+    public void AllTheRest(string numbers, int expected)
+    {
+        var result = _calculator.Add(numbers); Assert.Equal(expected, result);
     }
 }
