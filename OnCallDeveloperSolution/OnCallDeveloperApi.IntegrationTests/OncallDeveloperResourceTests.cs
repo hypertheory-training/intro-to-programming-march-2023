4e4578b2103050c92adce4f64ded31f6cbffd14b
Saleh <saleh6877@gmail.com>
2023-03-13 10:52:58 -0400

    MondayMorning3/13/2023

---
+++ OnCallDeveloperSolution/OnCallDeveloperApi.IntegrationTests/OncallDeveloperResourceTests.cs
@@ -0,0 +1,60 @@
+﻿
+using Alba;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+
+namespace OnCallDeveloperApi.IntegrationTests;
+
+public class OnCallDeveloperResourceTests
+{
+
+
+    [Fact]
+    public async Task CanGetOnCallDeveloperDuringBusinessHours()
+    {
+        // "Host" for our API our API will be up and running.
+        await using var host = await AlbaHost.For<Program>();
+
+        // Scenarios  - 
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/oncalldeveloper");
+            api.StatusCodeShouldBeOk();
+
+        });
+
+        var expectedResponse =
+            new GetOnCallDeveloperResponse("Michael N.", "555-1212", "mike@aol.com");
+
+        var actualResponse = response.ReadAsJson<GetOnCallDeveloperResponse>();
+
+        Assert.NotNull(actualResponse);
+
+        Assert.Equal(expectedResponse, actualResponse);
+
+    }
+    [Fact]
+    public async Task CanGetOnCallDeveloperOutsideBusinessHours()
+    {
+        // "Host" for our API our API will be up and running.
+        await using var host = await AlbaHost.For<Program>();
+
+        // Scenarios  - 
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/oncalldeveloper");
+            api.StatusCodeShouldBeOk();
+
+        });
+
+        var expectedResponse =
+            new GetOnCallDeveloperResponse("OnCallCorp Customer Service", "800 GOOD-LUCK", "oncall@company.com");
+
+        var actualResponse = response.ReadAsJson<GetOnCallDeveloperResponse>();
+
+        Assert.NotNull(actualResponse);
+
+        Assert.Equal(expectedResponse, actualResponse);
+
+    }
+}
---
+++ OnCallDeveloperSolution/OnCallDeveloperApi/Model/OnCallDeveloperApi.Models.cs
@@ -0,0 +1,11 @@
+﻿global using OnCallDeveloperApi.Models;
+global using Microsoft.AspNetCore.Mvc;
+
+
+namespace OnCallDeveloperApi.Models;
+
+public  record GetOnCallDeveloperResponse(
+string Name, string Phone, string Email)
+{
+
+}
